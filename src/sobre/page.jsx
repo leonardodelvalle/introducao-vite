35b6099360a4ff8cdbc04cda4a5962c86997f832
richardgvini16-del <richard.gvini16@gmail.com>
2026-03-02 14:05:54 -0300

    aula 27 02

---
+++ src/sobre/page.jsx
@@ -0,0 +1,16 @@
+function Sobre () {
+
+    return (
+
+        <div>
+            <h1>Sobre</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam unde praesentium officia culpa explicabo tenetur repellendus corporis impedit error? Suscipit, cumque perferendis. Culpa soluta nesciunt distinctio libero. Voluptas, sequi magni.</p>
+
+        </div>
+        
+    )
+
+}
+
+export default Sobre;
