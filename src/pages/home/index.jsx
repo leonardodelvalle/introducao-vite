fb3dc318cd896ca7a3934c11a56c51569abc75c6
richardgvini16-del <richard.gvini16@gmail.com>
2026-03-13 14:41:06 -0300

    aula 13 03

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,23 @@
-function App (){
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router";
+function home (){
+  const navigate = useNavigate();
+   
   return(
     <div>
       <h1>
       Aula de React com Vite JS
       </h1>
+
+      <button onClick={() => navigate ('/exemplo01/1')}>
+        Exemplo 1
+      </button>
+
+      <Link to="/exemplo02/2">Exemplo 2</Link>
+
     </div>
   );
 }
 
-export default App;
+export default home;
 
